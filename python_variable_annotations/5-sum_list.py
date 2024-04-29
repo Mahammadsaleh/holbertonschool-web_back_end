9423ebd3af9883bc28a4939bad8f20cd432c12d7
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-30 00:23:17 +0400

    list sum func handled

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Documentation"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Documentation of sum_list"""
     sum: float = 0
     for num in input_list:
