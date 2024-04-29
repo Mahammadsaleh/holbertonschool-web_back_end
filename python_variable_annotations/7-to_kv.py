315652498c6455c7f194c605875ec3441bad7258
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-30 01:38:35 +0400

    added

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ from typing import Union, Tuple
 
 Num = Union[int, float]
 
-def to_kv(k: str, v: Num) -> Tuple:
+def to_kv(k: str, v: Num) -> Tuple(str, Num):
     """Documentation"""
     return (k, v**2)
