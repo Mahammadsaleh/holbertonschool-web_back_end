cbb6a46bf547c5d18843266c81e9af9530f9a873
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-30 00:15:56 +0400

    list sum func handled

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Documentation"""
+
+
+def sum_list(input_list: list[float, ...]) -> float:
+    """Documentation of sum_list"""
+    sum: float = 0
+    for num in input_list:
+        sum = sum + num
+    return sum
