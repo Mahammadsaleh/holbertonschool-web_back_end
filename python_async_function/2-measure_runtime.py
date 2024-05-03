d3802b48fbf784c722837fcdcdb7ef858e0f9a2f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-04 02:53:24 +0400

    added

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,9 +4,10 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Wait for a random delay between 0 and max_delay"""
     start = asyncio.get_event_loop().time()
-    await wait_n(n, max_delay)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(wait_n(n, max_delay))
     end = asyncio.get_event_loop().time()
     return (end - start) / n
