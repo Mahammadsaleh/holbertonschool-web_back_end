51f615e08ee254379e81d3a6e393959b74edcabc
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-05 16:34:40 +0400

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 """Async Comprehensions"""
 import asyncio
 import random
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Async Comprehensions"""
     return [rand async for rand in async_generator()]
