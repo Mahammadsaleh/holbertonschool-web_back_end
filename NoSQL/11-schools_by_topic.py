8426c8cbb5cc34f8310e3423dd0e9c1a6e09e7aa
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-08 20:30:47 +0400

    ADDED

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     print("hello")
-    return mongo_collection.find({"topics": topic})
+    return [collection for collection in mongo_collection.find(
+        {"topics": topic}
+        )]
