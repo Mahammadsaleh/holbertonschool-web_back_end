abb1604d8cd20277cbafd4e53c21c84a9d31e610
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-08 20:28:42 +0400

    ADDED

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Listing all documents in a collection"""
-import pymongo
 
 
 def list_all(mongo_collection):
     """Listing all documents in a collection"""
     all = mongo_collection.find()
-    return all if all.count() > 0 else []
+    
+    if (len(list(all)) > 0):
+        return all
+    return []
