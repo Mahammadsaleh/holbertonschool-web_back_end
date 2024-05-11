f33b4dac8e095d883f75b8d581f890c34df14239
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-11 13:51:14 +0400

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,22 +26,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int), "Page" + \
-            " and page_size must be integers"
-        assert page > 0 and page_size > 0, "Page and" + \
-            "page_size must be positive"
+        """Get page from dataset"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         try:
             return self.dataset()[start:end]
         except IndexError:
             return []
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    index_range - calculates a start index and an end index
-    @page: number of page
-    @page_size: size of page
-    Return: tuple of size two containing a start index and an end index
-    """
-    return ((page - 1) * page_size, page * page_size)
