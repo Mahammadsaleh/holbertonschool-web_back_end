99414ca91f4a7a6076e827af8bb56575775969d1
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-12 13:58:16 +0400

    added

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,18 +1,4 @@
 /* eslint-disable */
-function do_someting(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    return initialNumber + expansion1989 + expansion2019;
   }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
-  return initialNumber + expansion1989 + expansion2019;
-}
-export default function getSumOfHoods(
-  initialNumber,
-  expansion1989,
-  expansion2019
-) {
-  return do_someting(initialNumber, expansion1989, expansion2019);
-}
