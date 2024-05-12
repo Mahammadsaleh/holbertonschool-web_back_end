dde226b5f4c2e1d786e832a2dea304bc8d4eb2d3
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-12 12:44:35 +0400

    Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function taskBlock(trueOrFalse) {
+  
+    if (trueOrFalse) {
+      var task = true;
+      var task2 = false;
+    }
+    var task = false;
+    var task2 = true;
+
+    return [task, task2];
+  }
