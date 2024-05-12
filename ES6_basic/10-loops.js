e2447cfb50e638ac90a10a9d542d2a43bb519ecc
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-12 17:08:28 +0400

    added

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const el of array) {
-    array.push(appendString + el);
+  for (let el of array) {
+    const idx = array.indexOf(el);
+    array[idx] = appendString + el;
   }
 
   return array;
