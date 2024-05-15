466a172c4def1a1aa0af52f1a54ac28946596581
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-15 15:29:07 +0400

    added

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
 export default class Currency {
     constructor(code ,name){
+        if (typeof code !== 'string') throw new TypeError('Code must be a string');
         this._code = code;
+        if (typeof name !== 'string') throw new TypeError('Name must be a string');
         this._name = name;
     }
     get code(){
