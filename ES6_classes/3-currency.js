3912629fa01af18a397bf5c22437b4a19138d5f9
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-15 15:30:29 +0400

    added

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -10,12 +10,14 @@ export default class Currency {
         return this._code;
     }
     set code(newCode){
+        if (typeof newCode !== 'string') throw new TypeError('Code must be a string');
         this._code = newCode;
     }
     get name(){
         return this._name;
     }
     set name(newName){
+        if (typeof newName !== 'string') throw new TypeError('Name must be a string');
         this._name = newName;
     }
     displayFullCurrency(params) {
