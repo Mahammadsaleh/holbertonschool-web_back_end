57509b02ce3ab719a0f5f913a14403f135bd6a1c
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-16 11:58:06 +0400

    calling this

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+    this.call();
+  }
+  call() {
+    return this;
+  }
+}
+export default Airport;
